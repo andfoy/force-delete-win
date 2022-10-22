a9936ff1997a53448c822ad0779286b5870d832a
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2022-10-21 19:13:32 -0500

    Make sure that coverage also covers C++ files
    Co-authored-by: Stephannie Jimenez Gacha <steff456@hotmail.com>

--- rust/build.rs
+++ rust/build.rs
@@ -18,12 +18,20 @@ fn main() {
     CFG.exported_header_dirs.push(&include_path);
     CFG.exported_header_dirs.push(&Path::new(&manifest_dir));
 
-    cxx_build::bridge("src/native.rs")
+    let mut build = cxx_build::bridge("src/native.rs");
+    let mut cc_build =
+        build
         .file("src/csrc/delete.cpp")
         .flag_if_supported("-std=gnu++14")
         .flag_if_supported("/EHsc")
         .define("_GLIBCXX_USE_CXX11_ABI", "0")
         .warnings(false)
-        .extra_warnings(false)
-        .compile("filedelete");
+        .extra_warnings(false);
+
+    if std::env::var("TRACE_NIGHTLY").is_ok() {
+        cc_build = cc_build.compiler("clang-cl")
+                           .flag("--coverage");
+    }
+
+    cc_build.compile("filedelete");
 }
