09990e33f32e7d9e891c5dcb6451a6e35245566b
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2022-10-24 13:28:53 -0500

    Debug inline

--- rust/src/csrc/delete.cpp
+++ rust/src/csrc/delete.cpp
@@ -194,6 +194,7 @@ void force_delete_file(rust::Vec<uint16_t> fname) {
                     }
                 }
 
+                std::wcout << file_path << " " << (fname_wstr == file_path) << " " << handle.ProcessId << std::endl;
                 if(fname_wstr == file_path) {
                     // std::wcout << file_path << " " << (fname_wstr == file_path) << " " << handle.ProcessId << std::endl;
                     CloseHandle(dupHandle);
